53b9ad199c4ff364c361d035c90b6da9984052bd
Xyl <georgetownsend24@yahoo.com>
2022-12-06 01:54:56 +0000

    did the more obvious thing

--- advent_5.py
+++ advent_5.py
@@ -11,18 +11,17 @@ for x in range(9):
         if lines[x][y+1] in string.ascii_uppercase:
             depot[int(y/4)].append(lines[x][y+1])
 
-
-"""for z in range(10,512):
-    nums = [int(s) for s in lines[z].split() if s.isdigit()]
-    for w in range(nums[0]):
-        depot[nums[2]-1] = [depot[nums[1]-1][0]] + depot[nums[2]-1]
-        del depot[nums[1]-1][0]"""
-
-
+# PART 1
 for z in range(10,512):
     nums = [int(s) for s in lines[z].split() if s.isdigit()]
-    depot[nums[2]-1] = depot[nums[1]-1][:nums[0]] + depot[nums[2]-1]
+    depot[nums[2]-1] = depot[nums[1]-1][:nums[0]][::-1] + depot[nums[2]-1]
     del depot[nums[1]-1][:nums[0]]
+
+# PART 2
+"""for z in range(10,512):
+    nums = [int(s) for s in lines[z].split() if s.isdigit()]
+    depot[nums[2]-1] = depot[nums[1]-1][:nums[0]] + depot[nums[2]-1]
+    del depot[nums[1]-1][:nums[0]]"""
 
 
 print(''.join([depot[x][0] for x in range(9)]))
